d45ce3ea695d924b732b71c48ccd74e71900bb55
KO13RN <50131793+KO13RN@users.noreply.github.com>
2023-04-21 18:14:42 +0700

    Update App.js
    ลบเทสออก

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function WelcomePage() {
           <li>ห้องสมุดวิจัยที่มีหนังสือและวารสารเกี่ยวกับกีฏวิทยา</li>
         </ul>
         <p className="welcome-message">
-          We can't wait to see you at Visual Insect Dome! Test
+          We can't wait to see you at Visual Insect Dome!
         </p>
         <a href="https://portal.furioos.com/share/npj7GzWXjfxDSMtqs?autoRun=true" target="_blank" rel="noopener noreferrer">
         <button onClick={handleButtonClick}>Click me to start</button>
